9c4a25ad49f9da69c7c82e19e87f19a48d6e66cd
Kylie Rose <kylierose@LI-BNE-OSX-01.local>
2019-07-31 15:30:24 +1000

    Updated packages and config

--- config/webpack.client.hot.js
+++ config/webpack.client.hot.js
@@ -34,8 +34,12 @@ module.exports = {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              modules: true,
-              localIdentName: '[local]___[hash:base64:5]'
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+                context: path.resolve(__dirname, 'src')
+                // hashPrefix: 'my-custom-hash'
+              }
             }
           },
           {
